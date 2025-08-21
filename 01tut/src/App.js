fda99b2af76f267a5517252ac3851a8b07578d0c
pawanYadavUser <pawan.yadav@iic.ac.in>
2025-08-21 09:04:48 +0530

    code-changes-for-react-learning

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
+import Counter from "./Counter";
 
 function App() {
   const name = "Dave";
@@ -16,6 +17,7 @@ function App() {
       <Header />
       <Content />
       <Footer />
+      <Counter />
     </div>
   );
 }
---
+++ 01tut/src/Counter.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Counter component</h2>
+      <p>Count : {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
